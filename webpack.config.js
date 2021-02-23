8ef78a5b7bf63a889300b502ff1a8812a2c90736
Racheal Colbert <rachealcolbert16@gmail.com>
2021-02-22 20:09:09 -0700

    other packages installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    entry: './public/js/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-    },
-    mode: 'development'
+  entry: "./public/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.bundle.js",
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ],
+  mode: "development",
 };
