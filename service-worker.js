6bccf5d86c6fef586e018fe827b6217029fb09eb
Racheal Colbert <rachealcolbert16@gmail.com>
2021-02-22 20:53:56 -0700

    fetch cache added to service-worker

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,24 @@ const FILES_TO_CACHE = [
     "./dist/main.bundle.js",
 ];
 
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else { // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+
+            // You can omit if/else for console.log & put one line below like this too.
+            // return request || fetch(e.request)
+        })
+    )
+})
+
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
